d4c9cb09dff48832bafd012f987a7fe3ebe8aea8
Amelia Foley <ameliamfoley@gmail.com>
2021-10-13 22:30:37 -0400

    cleaning and exploration

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -11,7 +11,7 @@ library(here) #to set paths
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+data_location <- here::here("data","raw_data","SympAct_Any_Pos.Rda")
 
 #load data. 
 #note that for functions that come from specific packages (instead of base R)
@@ -19,48 +19,36 @@ data_location <- here::here("data","raw_data","exampledata.xlsx")
 #package::function() that's not required one could just call the function
 #specifying the package makes it clearer where the function "lives",
 #but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+rawdata <- readRDS(data_location)
 
 #take a look at the data
 dplyr::glimpse(rawdata)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+#removing unneeded variables
+cleandata1 <- rawdata %>% select(-contains("Score"))
+cleandata2 <- cleandata1 %>% select(-contains("FluA"))  
+cleandata3 <- cleandata2 %>% select(-contains("FluB")) 
+cleandata4 <- cleandata3 %>% select(-contains("Total")) 
+cleandata5 <- cleandata4 %>% select(-contains("FluA")) 
+cleandata6 <- cleandata5 %>% select(-contains("Dxname")) 
+cleandata7 <- cleandata6 %>% select(-contains("Activity")) 
+cleandata8 <- cleandata7 %>% select(-"Unique.Visit")
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#After cleaning the data according to the instructions we have 32 variables, as expected. 
+#We know that one variable, BodyTemp, should be continuous. Let's check the class. 
+class(cleandata8$BodyTemp)
+#It is numeric, so we should be good to go. 
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
+#Remove missing values
+processeddata <- na.omit(cleandata8)
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
-
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
-
+#Now we have 730 observations of 32 variables, as expected. 
 # save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
 
 # location to save file
 save_data_location <- here::here("data","processed_data","processeddata.rds")
 
 saveRDS(processeddata, file = save_data_location)
 
-
+# for the rest of the analysis, our main continuous outcome of interest is 
+# BodyTemp, and our categorical outcome of interest is Nausea
